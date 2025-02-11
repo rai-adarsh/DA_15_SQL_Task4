e16186a6873a42bc2e8a94c0b81a018345e8fa60
AdarshRajput94 <adarshrai1994@gmail.com>
2025-02-11 12:51:48 +0530

    conditional task on enum type

--- task 4 enum & conditional query with enum.sql
+++ task 4 enum & conditional query with enum.sql
@@ -25,7 +25,7 @@ using order_status::order_status_enum;
 
 select * from orders;
 
-
+--- conditional query on string values
 select *,
 case
 when order_status in('Pending', 'Processing')then 'Active'
@@ -33,4 +33,56 @@ else 'Inactive'
 end as order_activity_status
 from orders;
 
+---conditional query on int/float values
+
+alter table orders 
+add column sales float;
+
+alter table orders 
+add column no_of_qty int;
+
+select * from orders;
+
+begin;
+
+update orders set
+ sales =case 
+when order_id = 1 then 255.6
+when order_id = 2 then 1565.22
+when order_id = 3 then 451.5
+when order_id = 4 then 877.9
+end,
+no_of_qty = case 
+when order_id = 1 then 2
+when order_id = 2 then 1
+when order_id = 3 then 4
+when order_id = 4 then 11
+end
+where order_id in (1, 2, 3, 4);
+
+
+rollback;
+
+create type math_type as enum ('sum', 'add', 'mul', 'div');
+
+begin;
+
+--- create function
+create or replace function basic_op(op math_type, sales float , no_of_qty int)
+returns float as $$
+DECLARE sumdata float;
+BEGIN
+if op = 'mul' THEN sumdata =sales * no_of_qty;
+elseif op= 'div' THEN sumdata= sales/no_of_qty;
+else sumdata = sales+no_of_qty;
+end if;
+return  sumdata;
+END;
+$$ LANGUAGE plpgsql
+
+select * , basic_op('mul',sales, no_of_qty) from orders ;
+
+select * , basic_op('div',sales, no_of_qty) from orders ;
+
+commit;
 
